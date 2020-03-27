08991a77a0e316c3157960d6a68bc2c4cf3646ba
terrydreyer <62754222+terrydreyer@users.noreply.github.com>
2020-03-27 08:47:10 -0500

    Update install.pp
    Updating from v8.4.2 to 8.6.0.2
    
    Thanks.

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class tripwire::install {
   $master_port = "--server-port ${::tripwire::master_port}"
   $passphrase = "--passphrase ${::tripwire::master_passcode}"
 
-  file { "${::tripwire::client_installdir}/te_agent_8.4.2_en_linux_x86_64/te_agent.bin" :
+  file { "${::tripwire::client_installdir}/te_agent_8.6.0.2_en_linux_x86_64/te_agent.bin" :
     ensure => 'file',
     owner  => 'root',
     group  => 'root',
@@ -15,8 +15,8 @@ class tripwire::install {
   }
 
   exec { 'installtripagt':
-    cwd       => "${::tripwire::client_installdir}/te_agent_8.4.2_en_linux_x86_64/",
-    path      => ["${::tripwire::client_installdir}/te_agent_8.4.2_en_linux_x86_64",'/bin','/usr/bin'],
+    cwd       => "${::tripwire::client_installdir}/te_agent_8.6.0.2_en_linux_x86_64/",
+    path      => ["${::tripwire::client_installdir}/te_agent_8.6.0.2_en_linux_x86_64",'/bin','/usr/bin'],
     command   => "te_agent.bin ${server_host} ${master_port} ${passphrase} --eula accept --silent   --enable-fips",
     creates   => "${::tripwire::client_installdir}/tripwire/te/agent/bin/",
     logoutput => true,
